9ade137386f5ccfb75bf83b7c0929e94cf325b02
nahidnstu12 <nahid.dev19@gmail.com>
2023-07-11 07:59:29 +0600

    template mern project

---
+++ template-mern/backend/server.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const path = require("path");
+
+// internal imports
+const userRoute = require("./route/userRoute");
+const authRoute = require("./route/authRoute");
+
+// app scafolding
+const app = express();
+
+// database connection
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connection Successful!"))
+  .catch((err) => console.log(err));
+
+// global middleware
+app.use(require("cors")());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(logger("dev"));
+
+// routeHandler
+app.use("/api/users", userRoute);
+app.use("/api/", authRoute);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  res.status(404);
+  res.json({
+    error: "Not found",
+  });
+  return;
+});
+
+// Error handlers
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    error: err.message,
+  });
+  return;
+});
+// console.log(process.env.MONGO_CONNECTION_STRING);
+
+//create server
+app.listen(process.env.PORT || 3100, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
+
+module.exports = app;
---
+++ template-mern/backend/validation/loginValidation.js
@@ -0,0 +1,36 @@
+const { check, validationResult } = require("express-validator");
+
+exports.doLoginValidators = [
+  check("username")
+    .isLength({
+      min: 3,
+    })
+    .withMessage("username or email is required"),
+  check("password").isLength({ min: 1 }).withMessage("Password is required"),
+];
+
+// object format
+exports.doLoginValidationHandler = (req, res, next) => {
+  const errors = validationResult(req);
+  const mappedErrors = errors.mapped();
+//   const errors = validationResult(req).formatWith(errorFormatter);
+   if (Object.keys(mappedErrors).length === 0) {
+    next();
+  } else {
+    // response the errors
+    res.status(400).json({
+      errors: mappedErrors,
+    });
+  }
+};
+
+// array format
+exports.doLoginValidationHandler2 = (req, res, next) => {
+  const errorFormatter = ({ msg, param }) => `${param} : ${msg}`;
+  const errors = validationResult(req).formatWith(errorFormatter);
+  if (!errors.isEmpty()) {
+    res.status(400).json(errors.array());
+  }
+  next();
+};
+
